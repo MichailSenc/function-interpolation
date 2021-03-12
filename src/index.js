bb71cc780bace962ba3e201aecc5b502fd42957c
Mixail <misha.senc@mail.ru>
2021-03-12 15:07:46 +0300

    main module

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import getInputData from "./components/get-intput-data";
+import basic from "./components/print-graphics";
+
+import {
+    getFunctionPoints,
+    getPolPoints,
+} from "./components/get-graphics-cord";
+
+import {
+    isChecked,
+    setListenersForCheckingData,
+} from "./components/check-input-data";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const optSize = document.querySelectorAll(".opt input[data-type-1]"),
+        functions = document.querySelectorAll(".opt input[type='checkbox']"),
+        del = document.querySelector("#del"),
+        nodes = document.querySelector("#nodes"),
+        button = document.querySelector("#button");
+
+    const offset = 10000;
+
+    setListenersForCheckingData({ optSize, nodes });
+
+    function start() {
+        if (!isChecked({ optSize, nodes })) {
+            console.log("Данные введены некорректно!");
+            return;
+        }
+
+        const dataSet = getInputData({ optSize, nodes, del });
+
+        const pointSet = [];
+        
+        pointSet.push(getFunctionPoints(dataSet, offset));
+        pointSet.push(getPolPoints(dataSet, offset));
+
+
+
+        basic("#container", pointSet, dataSet);
+
+        // //TODO: для каждой отмеченной функции построить график
+        // functions.forEach((fun) => {
+        //     if (fun.checked) {
+        //         // нарисовать граффик
+        //     }
+        // });
+    }
+
+    button.addEventListener("click", start);
+    document.addEventListener("keydown", (event) => {
+        if (event.code === "Enter") start();
+    });
+});
