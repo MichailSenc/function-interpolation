d33f9b79d030edc6dfc76e7a8a29a4ffcf638823
Mixail <misha.senc@mail.ru>
2021-03-12 15:11:11 +0300

    settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,17 @@
-'use strict';
+"use strict";
 
-let path = require('path');
+let path = require("path");
 
 module.exports = {
-  mode: 'production',
-  entry: './js/script.js',
-  output: {
-    filename: 'bundle.js',
-    path: __dirname + '/js'
-  },
-  watch: true,
+    mode: "production",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: __dirname + "/app",
+    },
+    watch: true,
 
-  devtool: "source-map",
+    devtool: "source-map",
 
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [['@babel/preset-env', {
-                debug: true,
-                corejs: 3,
-                useBuiltIns: "usage"
-            }]]
-          }
-        }
-      }
-    ]
-  }
+    module: {},
 };
